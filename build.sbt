c1ecf91615d1b8ab089420c5a1fa6c42de3f3646
Aurel Paulovic <aurel.paulovic@gmail.com>
2014-04-14 14:59:52 +0200

    first commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+organization := "com.aurelpaulovic"
+
+name := "scala-kata"
+
+version := "0.1"
+
+scalaVersion := "2.10.4"
+
+resolvers += "Sonatype (releases)" at "https://oss.sonatype.org/content/repositories/releases/"
+
+resolvers += Resolver.sonatypeRepo("snapshots")
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.3" % "test"
+
+scalacOptions ++= Seq( "-feature", "-deprecation", "-language:implicitConversions" )
---
+++ src/main/scala/com/aurelpaulovic/scala_kata/s_99/p01/package.scala
@@ -0,0 +1,41 @@
+package com.aurelpaulovic.scala_kata.s_99
+
+/*
+ * (*) Find the last element of a list
+ * 
+ * scala> last(List(1, 1, 2, 3, 5, 8))
+ * Int = 8
+ * 
+ */
+
+import scala.annotation.tailrec
+
+package object p01 {
+	/**
+	 * Gets the last element of the list using built-in
+	 * 
+	 * @param ls the list
+	 */
+	def last01[A](ls: List[A]): A = ls.last
+	
+	
+	/**
+	 * Gets the last element of the list using tail-recursive inner function
+	 * 
+	 * @param ls the list
+	 */
+	def last02[A](ls: List[A]): A = {
+	  @tailrec
+	  def last02Recursive[A](ls: List[A]): A = {
+	    ls match {
+	      case Nil => throw new NoSuchElementException
+	      case ele :: Nil => ele
+	      case _ :: tail => last02Recursive(tail)
+	    }
+	  }
+	  
+	  last02Recursive(ls)
+	}
+	
+	
+}
