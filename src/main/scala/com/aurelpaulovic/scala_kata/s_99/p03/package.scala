db9925314eae3e64c56c5433dac65c9090e0ce8a
Aurel Paulovic <aurel.paulovic@gmail.com>
2014-04-20 18:37:31 +0200

    tail-recursive implementations of nth element

--- src/main/scala/com/aurelpaulovic/scala_kata/s_99/p03/package.scala
+++ src/main/scala/com/aurelpaulovic/scala_kata/s_99/p03/package.scala
@@ -8,6 +8,24 @@ package com.aurelpaulovic.scala_kata.s_99
  * 
  */
 
+import scala.annotation.tailrec
+
 package object p03 {
 	def nth01[A](n: Int, ls: List[A]) = ls(n)
+	
+	@tailrec
+	def nth02[A](n: Int, ls: List[A]): A = {
+	  if(ls.isEmpty || n < 0) throw new IndexOutOfBoundsException
+	  else {
+	    if(n == 0) ls(0)
+	    else nth02(n-1, ls.tail)
+	  }
+	}
+	
+	@tailrec
+	def nth03[A](n: Int, ls: List[A]): A = (n, ls) match {
+	  case (0, ele :: _) => ele
+	  case (n, _ :: tail) if n > 0 => nth03(n - 1, tail)
+	  case _ => throw new IndexOutOfBoundsException
+	}
 }
--- src/test/scala/com/aurelpaulovic/scala_kata/s_99/p03/P03.scala
+++ src/test/scala/com/aurelpaulovic/scala_kata/s_99/p03/P03.scala
@@ -8,7 +8,9 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class P03 extends UnitSpec {
 	val funcs: List[(String, (Int, List[Int]) => Int)] = List( // we could use type Any, but than we would have to assert using == ... I think there is no way to specify it better :(
-			("nth01", nth01(_,_))
+			("nth01", nth01(_,_)),
+			("nth02", nth02(_,_)),
+			("nth03", nth03(_,_))
 		)
 		
 	for((k, f) <- funcs) {
@@ -27,6 +29,30 @@ class P03 extends UnitSpec {
 	    
 	    intercept[IndexOutOfBoundsException] {
 	      f(0, list)
+	    }
+	  }
+	}
+	
+	for((k, f) <- funcs) {
+	  "A " + k should "throw a IndexOutOfBoundsException on an index smaller than zero" in {
+	    val list = List(1,2,3)
+	    
+	    intercept[IndexOutOfBoundsException] {
+	      f(-1, list)
+	    }
+	  }
+	}
+	
+	for((k, f) <- funcs) {
+	  "A " + k should "throw a IndexOutOfBoundsException on an index greater than the number of elements in the list minus 1 " in {
+	    val list = List(1,2,3)
+	    
+	    intercept[IndexOutOfBoundsException] {
+	      f(3, list)
+	    }
+	    
+	    intercept[IndexOutOfBoundsException] {
+	      f(4, list)
 	    }
 	  }
 	}
